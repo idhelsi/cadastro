206372c78a76bd3214a9b707e6745f514eed3131
Helsi <helsi_@hotmail.com>
2023-09-24 12:15:20 -0400

    primeiro commit

---
+++ frontend/src/components/template/Footer.jsx
@@ -0,0 +1,10 @@
+import './Footer.css'
+import React from 'react'
+
+export default props =>
+    <footer className="footer">
+        <span>
+            Desenvolvido por 
+            <strong> <span className="text-danger">H</span>elsi</strong>
+        </span>
+    </footer>
